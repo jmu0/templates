7eeb65aa4b8f6b4daab499b8db0ecf4e9daa89ad
Jos Muysers <jos@muysers.nl>
2015-11-04 15:42:50 +0100

    templatemanager

--- templates.go
+++ templates.go
@@ -4,6 +4,7 @@ import (
 	// "fmt"
 	"errors"
 	"io/ioutil"
+	"log"
 	"strings"
 )
 
@@ -41,4 +42,45 @@ func (t *Template) Render() (string, error) {
 		}
 	}
 	return rendered, nil
+}
+
+type TemplateManager struct {
+	templatePath string
+	Cache        map[string]Template
+}
+
+func (tm *TemplateManager) Preload(path string) {
+	tm.Cache = make(map[string]Template)
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		log.Println(err)
+	} else {
+		for _, f := range files {
+			if f.IsDir() == false {
+				tPath := path + "/" + f.Name()
+				templ := Template{
+					Path: tPath,
+				}
+				if templ.Load("") == nil {
+					tm.Cache[tPath] = templ
+					log.Println("Preloading:", tPath)
+				}
+			}
+		}
+	}
+}
+func (tm *TemplateManager) GetTemplate(name string) (Template, error) {
+	path := tm.templatePath + "/" + name + ".html"
+	if tmpl, ok := tm.Cache[path]; ok {
+		log.Println("template from cache")
+		return tmpl, nil
+	}
+	tmpl := Template{}
+	err := tmpl.Load(path)
+	if err != nil {
+		log.Println("no template:", err)
+		return tmpl, err
+	}
+	log.Println("new template")
+	return tmpl, nil
 }
